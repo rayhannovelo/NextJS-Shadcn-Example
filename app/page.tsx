90a0064c8f760baf82fdf34090bde29f9e57db03
Muhammad Rayhan Novelo <rayhan.novelo@gmail.com>
2024-06-27 16:04:16 +0700

    add login form

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,111 @@
-import Image from "next/image"
+"use client"
+
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+import axios from "axios"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+const formSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+})
 
 export default function Page() {
-  return <main>Next.js Shadcn Example</main>
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  })
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL
+
+    try {
+      const response = await axios.post(`${backendUrl}/auth`, values)
+
+      if (response.data.success) {
+        alert("Login success!")
+      } else {
+        alert("Login failed!")
+      }
+      // Handle success, e.g., redirect or store token
+    } catch (error) {
+      alert("Login failed!")
+      // Handle error, show message to user
+    }
+  }
+
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Card className="sm:min-w-96">
+        <CardHeader>
+          <CardTitle className="text-center text-4xl">Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input type="text" placeholder="Username" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button className="w-full" type="submit">
+                Submit
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter className="flex-col">
+          <p className="block">© 2024 Powered By Ray</p>
+          <p className="block">All rights reserved</p>
+        </CardFooter>
+      </Card>
+    </div>
+  )
 }
