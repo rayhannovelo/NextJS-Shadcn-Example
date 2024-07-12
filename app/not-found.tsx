7980711fee647ec45b825ef8ff6a928f49e1274d
Muhammad Rayhan Novelo <rayhan.novelo@gmail.com>
2024-07-12 08:56:12 +0700

    add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <>
+      <div className="flex justify-center items-center gap-3 h-screen w-screen">
+        <p className="text-3xl border-r-2 border-black pr-3">404</p>
+        <p className="text-xl">Not Found</p>
+      </div>
+    </>
+  )
+}
